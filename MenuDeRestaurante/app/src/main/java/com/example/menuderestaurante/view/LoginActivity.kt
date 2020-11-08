1864ea4242ea89c3d6573e8197ee086cc656bbf0
Sara Alves <saraalves14@hotmail.com>
2020-11-08 13:09:12 -0300

    Desenvolvimento de navegação simples

--- MenuDeRestaurante/app/src/main/java/com/example/menuderestaurante/view/LoginActivity.kt
+++ MenuDeRestaurante/app/src/main/java/com/example/menuderestaurante/view/LoginActivity.kt
@@ -1,12 +1,19 @@
 package com.example.menuderestaurante.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.menuderestaurante.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        findViewById<Button>(R.id.btnRegisterLogin).setOnClickListener{
+            intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- MenuDeRestaurante/app/src/main/java/com/example/menuderestaurante/view/RegisterActivity.kt
+++ MenuDeRestaurante/app/src/main/java/com/example/menuderestaurante/view/RegisterActivity.kt
@@ -1,12 +1,19 @@
 package com.example.menuderestaurante.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.menuderestaurante.R
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        findViewById<Button>(R.id.btnLoginRegister).setOnClickListener{
+            intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
